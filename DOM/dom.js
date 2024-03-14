070881ee0358d3367724b8494289df4a6fa77784
Danielroxs <danroxsll@gmail.com>
2024-03-13 19:24:01 -0600

    Evento DOMContentLoaded y focus & blur

---
+++ DOM/dom.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const h1 = document.querySelector('#titulo');
+    console.log(h1);
+    h1.textContent = 'Segundo titulo';
+})
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('El documento esta totalmente cargado')
+})
