5ed96c092c8c9eaca3990020d0ce617b8e22750f
Danielroxs <danroxsll@gmail.com>
2024-03-22 15:09:42 -0600

    Clases y herencia

--- DOM/POO/clases.js
+++ DOM/POO/clases.js
@@ -3,22 +3,81 @@
 // Crear una clase
 class Persona {
     
-    constructor (name = 'sin nombre', lastname = 'sin apellido') {
-        this.nombre = name;
-        this.apellido = lastname;
+    constructor (name = 'sin nombre', lastname = 'sin apellido', localidad = 'Mexico') {
+        this._nombre = name;
+        this._apellido = lastname;
+        this._localidad = localidad;
     } // fin del constructor
+
+    // Metodo get es para obtener algo
+    get getNombre () {
+        return this._nombre;
+    }
+
+    // Metodo set es para cambiar un valor a una propiedad
+    set setNombre (name) {
+        this._nombre = name;
+    }
+
+    get getApellido () {
+        return this._apellido;
+    }
+
+    set setApellido (apellido) {
+        this._apellido = apellido;
+    }
+
+    get getLocalidad () {
+        return this._localidad;
+    }
+
+    set setLocalidad (localidad) {
+        this._localidad = localidad;
+    }
+
+    get getTodo () {
+        return [this._nombre, this._apellido, this._localidad];
+    }
 
 } // fin de la clase Persona
 
+class Empleado extends Persona {
+    constructor(name, lastname, localidad, departamento, sueldoBasico) {
+        super (name, lastname, localidad);
+        this._departamento = departamento;
+        this._sueldoBasico = sueldoBasico;
+
+    }
+
+    set setDepartamento (departamento) {
+        this._departamento = departamento;
+    }
+
+    set setSueldoBasico (sueldoBasico) {
+        this._sueldoBasico = sueldoBasico;
+    }
+
+    get getDepartamento () {
+        return this._departamento;
+    }
+
+    get getSueldoBasico () {
+        return this._sueldoBasico;
+    }
+    
+}
+
 let cliente02 = new Persona('Mark','Markinson');
 cliente02.tel = 33445566
-console.log(cliente02)
 
-let cliente03 = new Persona('Juan','Perez');
+cliente02.setNombre = 'Juan'
+console.log(cliente02.getTodo)
+
+/* let cliente03 = new Persona('Juan','Perez');
 console.log(cliente03)
 
 let cliente04 = new Persona('Dan', 'Rox');
 console.log(cliente04)
 
 let cliente05 = new Persona();
-console.log(cliente05)
+console.log(cliente05) */
---
+++ DOM/POO/herencia.js
@@ -0,0 +1,117 @@
+// Las clases son moldes para la creacion de objetos, me permiten crear un monton de objetos similares apoyandome en su molde
+
+// Crear una clase
+class Persona {
+
+    constructor(name = 'sin nombre', lastname = 'sin apellido', localidad = 'Mexico') {
+        this._nombre = name;
+        this._apellido = lastname;
+        this._localidad = localidad;
+    } // fin del constructor
+
+    // Metodo get es para obtener algo
+    get getNombre() {
+        return this._nombre;
+    }
+
+    // Metodo set es para cambiar un valor a una propiedad
+    set setNombre(name) {
+        this._nombre = name;
+    }
+
+    get getApellido() {
+        return this._apellido;
+    }
+
+    set setApellido(apellido) {
+        this._apellido = apellido;
+    }
+
+    get getLocalidad() {
+        return this._localidad;
+    }
+
+    set setLocalidad(localidad) {
+        this._localidad = localidad;
+    }
+
+    get getTodo() {
+        return `Nombre y apellido: ${this._nombre} ${this._apellido} y vivo en ${this._localidad}.`;
+    }
+
+} // fin de la clase Persona
+
+class Empleado extends Persona {
+    constructor(name, lastname, localidad, departamento, sueldoBasico) {
+        super(name, lastname, localidad);
+        this._departamento = departamento;
+        this._sueldoBasico = sueldoBasico;
+
+    }
+
+    set setDepartamento(departamento) {
+        this._departamento = departamento;
+    }
+
+    set setSueldoBasico(sueldoBasico) {
+        this._sueldoBasico = sueldoBasico;
+    }
+
+    get getDepartamento() {
+        return this._departamento;
+    }
+
+    get getSueldoBasico() {
+        return this._sueldoBasico;
+    }
+
+}
+
+class Ejecutivo extends Empleado {
+    constructor(name, lastname, localidad, departamento, sueldoBasico, beneficio, obraSocial) {
+        super (name, lastname, localidad, departamento, sueldoBasico);
+        this._beneficio = beneficio;
+        this._obraSocial = obraSocial;
+    }
+
+    // GETTERS
+
+    get getBeneficio () {
+        return this._beneficio;
+    }
+
+    get getObraSocial () {
+        return this._obraSocial;
+    }
+
+    // SETTERS
+
+    set setBeneficio (beneficio){
+        this._beneficio = beneficio;
+    }
+
+    set setObraSocial(obraSocial) {
+        this._obraSocial = obraSocial;
+    }
+}
+
+let persona01 = new Persona('Mark', 'Markinson');
+let empleado01 = new Empleado('Jorge', 'Perez', 'Bs As', 'Ventas', 56000)
+let empleado02 = new Empleado('Domingo', 'Sarmiento', 'Formosa', 'Administracion', 86000)
+let ejecutivo01 = new Ejecutivo('Romina', 'Manguel', 'Capital Federal', 'RRHH', 19600, 'Toyota 2022', 'Galeno Oro');
+
+console.table(empleado01);
+console.table(empleado02);
+console.log(empleado02.getNombre);
+empleado02.setLocalidad = 'Villa Hermosa'
+console.log(empleado02.getTodo);
+
+console.table(empleado01);
+console.log(ejecutivo01.getNombre);
+console.log(ejecutivo01.getDepartamento);
+console.log(ejecutivo01.getObraSocial);
+
+ejecutivo01.setDepartamento = 'Ventas'
+ejecutivo01.setNombre = 'Johan';
+ejecutivo01.setBeneficio = 'Lexus 2021';
+console.table(ejecutivo01);
