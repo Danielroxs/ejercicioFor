aafcef7aa9bf1c769a39ded89bae1b69e605c4b3
Danielroxs <danroxsll@gmail.com>
2024-03-30 19:29:53 -0600

    promesas funcion, info error

--- DOM/POO/codigoAsincrono/promesas.js
+++ DOM/POO/codigoAsincrono/promesas.js
@@ -1,24 +1,50 @@
-const primerPromesa = new Promise((resolve, reject) => {
-    const num = Math.random();
-    console.log(num);
-    if(num < 0.5) {
-        return resolve()
-    } return reject();
+console.log('Comenzo el programa');
+
+const funcPromesa = (url) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const paginas = {
+                '/users' : [
+                    {id: 1, usuario:'Juan'},
+                    {id: 2, usuario:'Pedro'},
+                ],
+                '/about' :  'Esta pagina es de ...'
+            };
+
+            const datos = paginas[url];
+
+            if(datos) {
+                return resolve({status: 200, datos});
+            }
+            return reject({status: 404});
+        }, 5000)
+
+    })
+}
+
+
+
+
+funcPromesa('/about').then((respuesta) => {
+    console.log(respuesta.status);
+    console.log(respuesta.datos);
+    console.log('True');
+}).catch((respuesta) => {    
+    console.log(respuesta.status)             
+    console.log('False')
 })
 
-primerPromesa.then(() => {   
+/* primerPromesa().then(() => {
     console.log('True');
 });
 
-primerPromesa.catch(() => {
-    console.log('False')
-})
-
-/* primerPromesa.then(() => { 
-    console.log('True');
-}).catch(() => {                 Tambien puede verse asi la promesa
+primerPromesa().catch(() => {
     console.log('False')
 }) */
 
-
 // Simulamos que estamos haciendo una consulta al servidor, arrojando un mensaje si obtuvimos respuesta
+
+console.log('Continua el programa');
+console.log('Continua el programa');
+console.log('Continua el programa');
+console.log('Continua el programa');
