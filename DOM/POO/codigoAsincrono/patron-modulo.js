2a1a4d7e78f7042e00c832ac74fb5b194d3578ce
Danielroxs <danroxsll@gmail.com>
2024-04-11 15:54:30 -0600

    Minifier

---
+++ DOM/POO/codigoAsincrono/patron-modulo.js
@@ -0,0 +1,11 @@
+'use strict';
+
+(function () {
+    console.log('Hola');
+    const nombres = ['Marcos', 'Juan'];
+    console.log(nombres)
+})();
+
+(() => {
+    console.log('Hola de nuevo');
+})();
