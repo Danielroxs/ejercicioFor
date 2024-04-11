09e1c578c6c862fc55ef80e8a05b33cfef79ebac
Danielroxs <danroxsll@gmail.com>
2024-04-10 19:12:22 -0600

    Use Strict-Buenas Practicas

---
+++ DOM/POO/codigoAsincrono/app.js
@@ -0,0 +1,21 @@
+'use strict'
+
+console.log('use strict');
+let x = 38;
+console.log(x);
+
+function misF () {
+    let y = 0.33;
+    console.log(y);
+}
+
+misF();
+
+let a = [];
+console.log(a)
+
+function objeto () {
+    console.log(this)
+}
+
+objeto()
